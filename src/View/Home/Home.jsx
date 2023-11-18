45128bfec7c5f6459d21b929cbc8cbc98f9c2f55
matiasvincent2002 <72351361+matiasvincent2002@users.noreply.github.com>
2023-11-18 18:40:32 -0300

    agregando destacados

--- src/View/Home/Home.jsx
+++ src/View/Home/Home.jsx
@@ -1,12 +1,228 @@
-import React from 'react'
-import style from './Home.module.css'
-import Header from '../../components/Header/Header'
+// Home.jsx
+import React from 'react';
+import style from './Home.module.css';
+import Header from '../../components/Header/Header';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import ProductCard from '../../components/Product/Product.jsx'; // Ajusta la ruta según tu estructura
+
+const featuredProducts = [
+  {
+    titulo: "Sandalias Bajas Romanas Confort Beira Rio Tipcalzado 8488",
+    coloresDisponibles: ["Blanco", "Negro"], // Obtén estos valores del contenido real
+    imagenes: [
+      "https://http2.mlstatic.com/D_NQ_NP_910874-MLA72063076871_102023-W.webp",
+      "https://http2.mlstatic.com/D_683974-MLA72063076873_102023-O.jpg",
+      "https://http2.mlstatic.com/D_970941-MLA72005652816_102023-O.jpg",
+    ],
+    enlace: "https://articulo.mercadolibre.com.ar/MLA-1390996815-sandalias-bajas-romanas-confort-beira-rio-tipcalzado-8488-_JM#position=1&amp;search_layout=grid&amp;type=item&amp;tracking_id=ab3f2efe-e231-406e-b85f-48d1a2c87e3f",
+    calificacion: {
+      valor: 4.5,
+      cantidadOpiniones: 4,
+    },
+    precio: {
+      montoTotal: 25999,
+      numeroCuotas: 12,
+      montoPorCuota: 5219,
+    },
+    envioGratis: true,
+  },
+   {
+    titulo: "Sandalias Bajas Beira Rio Confort Tip Calzados Hot 8384",
+    coloresDisponibles: ["Negro", "Plata"],
+    imagenes: [
+      "https://http2.mlstatic.com/D_NQ_NP_850416-MLA72771210925_112023-W.webp",
+      "https://http2.mlstatic.com/D_814739-MLA72771366273_112023-O.jpg",
+      "https://http2.mlstatic.com/D_801671-MLA72697273050_112023-O.jpg",
+      "https://http2.mlstatic.com/D_729130-MLA72697430320_112023-O.jpg",
+    ],
+    enlace: "https://articulo.mercadolibre.com.ar/MLA-1394995997-sandalias-bajas-beira-rio-confort-tip-calzados-hot-8384-_JM#position=3&amp;search_layout=grid&amp;type=item&amp;tracking_id=ab3f2efe-e231-406e-b85f-48d1a2c87e3f",
+    calificacion: {
+      valor: null, // Valor de la calificación no proporcionado
+      cantidadOpiniones: null, // Cantidad de opiniones no proporcionada
+    },
+    precio: {
+      montoTotal: 21999,
+      numeroCuotas: 12,
+      montoPorCuota: 4416,
+    },
+    envioGratis: true,
+  },
+  {
+    titulo: "Sandalias De Fiesta Plataforma Vizzano. Tipcalzado Hot 6210",
+    calificacion: {
+      valor: 4.8,
+      cantidadOpiniones: 4,
+    },
+    precio: {
+      montoTotal: 38999,
+      numeroCuotas: 12,
+      montoPorCuota: 7166,
+    },
+    envioGratis: true,
+    imagenes: [
+      "https://http2.mlstatic.com/D_NQ_NP_802607-MLA71102699963_082023-W.webp",
+      "https://http2.mlstatic.com/D_679650-MLA71064714158_082023-O.jpg",
+    ],
+    enlace: "https://articulo.mercadolibre.com.ar/MLA-1383356595-sandalias-de-fiesta-plataforma-vizzano-tipcalzado-hot-6210-_JM#position=4&amp;search_layout=grid&amp;type=item&amp;tracking_id=ab3f2efe-e231-406e-b85f-48d1a2c87e3f",
+  },
+  {
+    titulo: "Sandalias Bajas Romanas Confort Beira Rio Tipcalzado 8488",
+    coloresDisponibles: ["Blanco", "Negro"], // Obtén estos valores del contenido real
+    imagenes: [
+      "https://http2.mlstatic.com/D_NQ_NP_910874-MLA72063076871_102023-W.webp",
+      "https://http2.mlstatic.com/D_683974-MLA72063076873_102023-O.jpg",
+      "https://http2.mlstatic.com/D_970941-MLA72005652816_102023-O.jpg",
+    ],
+    enlace: "https://articulo.mercadolibre.com.ar/MLA-1390996815-sandalias-bajas-romanas-confort-beira-rio-tipcalzado-8488-_JM#position=1&amp;search_layout=grid&amp;type=item&amp;tracking_id=ab3f2efe-e231-406e-b85f-48d1a2c87e3f",
+    calificacion: {
+      valor: 4.5,
+      cantidadOpiniones: 4,
+    },
+    precio: {
+      montoTotal: 25999,
+      numeroCuotas: 12,
+      montoPorCuota: 5219,
+    },
+    envioGratis: true,
+  },
+   {
+    titulo: "Sandalias Bajas Beira Rio Confort Tip Calzados Hot 8384",
+    coloresDisponibles: ["Negro", "Plata"],
+    imagenes: [
+      "https://http2.mlstatic.com/D_NQ_NP_850416-MLA72771210925_112023-W.webp",
+      "https://http2.mlstatic.com/D_814739-MLA72771366273_112023-O.jpg",
+      "https://http2.mlstatic.com/D_801671-MLA72697273050_112023-O.jpg",
+      "https://http2.mlstatic.com/D_729130-MLA72697430320_112023-O.jpg",
+    ],
+    enlace: "https://articulo.mercadolibre.com.ar/MLA-1394995997-sandalias-bajas-beira-rio-confort-tip-calzados-hot-8384-_JM#position=3&amp;search_layout=grid&amp;type=item&amp;tracking_id=ab3f2efe-e231-406e-b85f-48d1a2c87e3f",
+    calificacion: {
+      valor: null, // Valor de la calificación no proporcionado
+      cantidadOpiniones: null, // Cantidad de opiniones no proporcionada
+    },
+    precio: {
+      montoTotal: 21999,
+      numeroCuotas: 12,
+      montoPorCuota: 4416,
+    },
+    envioGratis: true,
+  },
+  {
+    titulo: "Sandalias De Fiesta Plataforma Vizzano. Tipcalzado Hot 6210",
+    calificacion: {
+      valor: 4.8,
+      cantidadOpiniones: 4,
+    },
+    precio: {
+      montoTotal: 38999,
+      numeroCuotas: 12,
+      montoPorCuota: 7166,
+    },
+    envioGratis: true,
+    imagenes: [
+      "https://http2.mlstatic.com/D_NQ_NP_802607-MLA71102699963_082023-W.webp",
+      "https://http2.mlstatic.com/D_679650-MLA71064714158_082023-O.jpg",
+    ],
+    enlace: "https://articulo.mercadolibre.com.ar/MLA-1383356595-sandalias-de-fiesta-plataforma-vizzano-tipcalzado-hot-6210-_JM#position=4&amp;search_layout=grid&amp;type=item&amp;tracking_id=ab3f2efe-e231-406e-b85f-48d1a2c87e3f",
+  },
+  {
+    titulo: "Sandalias Bajas Romanas Confort Beira Rio Tipcalzado 8488",
+    coloresDisponibles: ["Blanco", "Negro"], // Obtén estos valores del contenido real
+    imagenes: [
+      "https://http2.mlstatic.com/D_NQ_NP_910874-MLA72063076871_102023-W.webp",
+      "https://http2.mlstatic.com/D_683974-MLA72063076873_102023-O.jpg",
+      "https://http2.mlstatic.com/D_970941-MLA72005652816_102023-O.jpg",
+    ],
+    enlace: "https://articulo.mercadolibre.com.ar/MLA-1390996815-sandalias-bajas-romanas-confort-beira-rio-tipcalzado-8488-_JM#position=1&amp;search_layout=grid&amp;type=item&amp;tracking_id=ab3f2efe-e231-406e-b85f-48d1a2c87e3f",
+    calificacion: {
+      valor: 4.5,
+      cantidadOpiniones: 4,
+    },
+    precio: {
+      montoTotal: 25999,
+      numeroCuotas: 12,
+      montoPorCuota: 5219,
+    },
+    envioGratis: true,
+  },
+   {
+    titulo: "Sandalias Bajas Beira Rio Confort Tip Calzados Hot 8384",
+    coloresDisponibles: ["Negro", "Plata"],
+    imagenes: [
+      "https://http2.mlstatic.com/D_NQ_NP_850416-MLA72771210925_112023-W.webp",
+      "https://http2.mlstatic.com/D_814739-MLA72771366273_112023-O.jpg",
+      "https://http2.mlstatic.com/D_801671-MLA72697273050_112023-O.jpg",
+      "https://http2.mlstatic.com/D_729130-MLA72697430320_112023-O.jpg",
+    ],
+    enlace: "https://articulo.mercadolibre.com.ar/MLA-1394995997-sandalias-bajas-beira-rio-confort-tip-calzados-hot-8384-_JM#position=3&amp;search_layout=grid&amp;type=item&amp;tracking_id=ab3f2efe-e231-406e-b85f-48d1a2c87e3f",
+    calificacion: {
+      valor: null, // Valor de la calificación no proporcionado
+      cantidadOpiniones: null, // Cantidad de opiniones no proporcionada
+    },
+    precio: {
+      montoTotal: 21999,
+      numeroCuotas: 12,
+      montoPorCuota: 4416,
+    },
+    envioGratis: true,
+  },
+  {
+    titulo: "Sandalias De Fiesta Plataforma Vizzano. Tipcalzado Hot 6210",
+    calificacion: {
+      valor: 4.8,
+      cantidadOpiniones: 4,
+    },
+    precio: {
+      montoTotal: 38999,
+      numeroCuotas: 12,
+      montoPorCuota: 7166,
+    },
+    envioGratis: true,
+    imagenes: [
+      "https://http2.mlstatic.com/D_NQ_NP_802607-MLA71102699963_082023-W.webp",
+      "https://http2.mlstatic.com/D_679650-MLA71064714158_082023-O.jpg",
+    ],
+    enlace: "https://articulo.mercadolibre.com.ar/MLA-1383356595-sandalias-de-fiesta-plataforma-vizzano-tipcalzado-hot-6210-_JM#position=4&amp;search_layout=grid&amp;type=item&amp;tracking_id=ab3f2efe-e231-406e-b85f-48d1a2c87e3f",
+  },
+ 
+];
+
+const featuredCarouselSettings = {
+  infinite: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
+
 const Home = () => {
   return (
     <div className={style.homeContainer}>
-      <Header/>
-    </div>
-  )
-}
+      <Header />
+      {/* Otras secciones de la página Home */}
 
-export default Home
+      {/* Sección de destacados */}
+      <div className={style.featuredSection}>
+        <h2>Destacados</h2>
+        <Slider {...featuredCarouselSettings}>
+          {featuredProducts.map((product) => (
+            <div key={product.id} className={style.featuredItem}>
+              <ProductCard product={product} />
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 // Header.jsx
 import React, { useState } from 'react';
-import { Carousel } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Carousel, Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import Slider from 'react-slick'; // Importa Slider de react-slick
 import style from './Header.module.css';
 import img1 from '../../assets/headerimg.jpg';
 import img2 from '../../assets/headerimg2.jpg';
 import img3 from '../../assets/headerimg3.jpg';
+
 
 const imagePaths = {
   image1: img1,
@@ -14,6 +17,7 @@ const imagePaths = {
   // Agrega más imágenes según sea necesario
 };
 
+
 const Header = () => {
   const [index, setIndex] = useState(0);
 
@@ -21,15 +25,53 @@ const Header = () => {
     setIndex(selectedIndex);
   };
 
+  // Configuración del carousel de destacados
+ 
+
   return (
     <div className={style.header}>
-      <Carousel activeIndex={index} onSelect={handleSelect} className={`w-100 ${style.customCarousel}`} interval={3000} slide>
-        {Object.values(imagePaths).map((path, idx) => (
-          <Carousel.Item key={idx}>
-            <img className={`d-block ${style.image}`} src={path} alt={`Imagen ${idx + 1}`} />
-          </Carousel.Item>
-        ))}
-      </Carousel>
+      {/* Navbar */}
+      <div className={style.customNavbar}>
+        {/* Logo */}
+        <div className={style.logo}>Tip</div>
+
+        {/* Barra de búsqueda */}
+        <div className={`${style.searchBar}`}>
+          <input type="text" placeholder="Buscar" className={style.searchInput} />
+          <button className={style.searchButton}>
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </div>
+
+        {/* Opciones de navegación */}
+        <div className={`${style.navOptions}`}>
+          <a href="#home">Inicio</a>
+          <a href="#acerca">Acerca de</a>
+          <a href="#servicios">Servicios</a>
+          <a href="#contacto">Contacto</a>
+        </div>
+      </div>
+
+      {/* Carousel principal */}
+      <div className={style.carouselContainer}>
+        <Carousel
+        
+          activeIndex={index}
+          onSelect={handleSelect}
+          className={`w-100 ${style.customCarousel}`}
+          interval={3000}
+          slide
+        >
+          {Object.values(imagePaths).map((path, idx) => (
+            <Carousel.Item key={idx}>
+              <img className={`d-block ${style.image}`} src={path} alt={`Imagen ${idx + 1}`} />
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </div>
+
+      {/* Sección de destacados */}
+      
     </div>
   );
 };
